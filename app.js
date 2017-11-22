b969347bf1935c7428362b69d4635035146631fe
xx404 <xx404@msn.com>
2017-11-22 11:29:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(
 
     connection(mysql,{
 
-        host: '172.30.235.5', // host mysql service
+        host: '172.30.87.231', // host mysql service
         user: 'dbuser',
         password : 'password',
         port : 3306, // port mysql service
